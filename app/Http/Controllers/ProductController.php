fc8419178db84bb74b89be821f940b15517f5a5a
= <=>
2020-04-17 11:52:37 +0545

    Product update handled

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -91,7 +91,13 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product->detail = $request->description;
+        unset($request['description']);
+        $product->update($request->all());
+        
+        return response([
+            'data' => new ProductResource($product)
+        ], Response::HTTP_CREATED);
     }
 
     /**
