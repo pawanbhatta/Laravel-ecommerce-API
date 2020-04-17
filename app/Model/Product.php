c84f70fe5d2350485ae26a5cbab7a59646f04efb
= <=>
2020-04-17 07:31:24 +0545

    build relationship between Product and Review

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,10 +3,16 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
 
 class Product extends Model
 {
     protected $table = 'products';
     protected $fillable = ['name', 'detail', 'price', 'stock', 'discount'];
     protected $primaryKey = 'id';
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -3,10 +3,16 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Product;
 
 class Review extends Model
 {
     protected $table = 'reviews';
     protected $fillable = ['customer', 'review', 'star', 'product_id'];
     protected $primaryKey = 'id';
+
+    public function products()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
